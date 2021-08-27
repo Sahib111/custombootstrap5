8fb26c6057ccfbbbedb3bc871fbb2b58dba146ca
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-08-27 19:49:15 +0530

    Update universal_script.js

--- bootstrap_contact_forms/universal_script.js
+++ bootstrap_contact_forms/universal_script.js
@@ -1,2 +1,3 @@
-import '.js/bootstrap.js'
-import '.js/bootstrap_bundle'
+import '.js/bootstrap.js';
+import '.js/bootstrap_bundle';
+import url("https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js");
